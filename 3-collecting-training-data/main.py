bf0509876ff56a4e1a3c5f6fe5af22fc39a66772
Łukasz Piłatowski <lukpila29@gmail.com>
2020-08-18 15:06:33 +0200

    add images storage

--- 3-collecting-training-data/main.py
+++ 3-collecting-training-data/main.py
@@ -1,4 +1,7 @@
 import argparse
+from pathlib import Path
+from multiprocessing import Process
+from uuid import uuid4
 
 import consts.resource_paths
 import cv2
@@ -9,9 +12,16 @@ if not depthai.init_device(consts.resource_paths.device_cmd_fpath):
     raise RuntimeError("Error initializing device. Try to reset it.")
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--threshold', default=0.1, type=float, help="Maximum difference between packet timestamps to be considered as synced")
+parser.add_argument('-t', '--threshold', default=0.03, type=float, help="Maximum difference between packet timestamps to be considered as synced")
 parser.add_argument('-f', '--fps', default=30, type=int, help="FPS of the cameras")
+parser.add_argument('-p', '--path', default="data", type=str, help="Path where to store the captured data")
+parser.add_argument('-d', '--dirty', action='store_true', default=False, help="Allow the destination path not to be empty")
 args = parser.parse_args()
+
+dest = Path(args.path).resolve().absolute()
+if dest.exists() and len(list(dest.glob('*'))) != 0 and not args.dirty:
+    raise ValueError(f"Path {dest} contains {len(list(dest.glob('*')))} files. Either specify new path or use \"--dirty\" flag to use current one")
+dest.mkdir(parents=True, exist_ok=True)
 
 p = depthai.create_pipeline(config={
     "streams": [
@@ -44,6 +54,7 @@ if p is None:
 
 latest_left = None
 lr_pairs = {}
+procs = []
 
 # https://stackoverflow.com/a/7859208/5494277
 def step_norm(value):
@@ -52,31 +63,39 @@ def seq(packet):
     return packet.getMetadata().getSequenceNum()
 def tst(packet):
     return packet.getMetadata().getTimestamp()
+def store_frames(left, right, rgb):
+    global procs
+    frames_path = dest / Path(str(uuid4()))
+    frames_path.mkdir(parents=False, exist_ok=False)
+    new_procs = [
+        Process(target=cv2.imwrite, args=(str(frames_path / Path("left.png")), left)),
+        Process(target=cv2.imwrite, args=(str(frames_path / Path("right.png")), right)),
+        Process(target=cv2.imwrite, args=(str(frames_path / Path("rgb.png")), rgb)),
+    ]
+    for proc in new_procs:
+        proc.start()
+    procs += new_procs
 
 while True:
     data_packets = p.get_available_data_packets()
 
     for packet in data_packets:
         print(packet.stream_name, packet.getMetadata().getTimestamp(), packet.getMetadata().getSequenceNum(), packet.getMetadata().getCameraName())
-        if packet.stream_name == 'previewout':
-            data = packet.getData()
-            data0 = data[0, :, :]
-            data1 = data[1, :, :]
-            data2 = data[2, :, :]
-            frame = cv2.merge([data0, data1, data2])
-        elif packet.stream_name == 'left' or packet.stream_name == 'right' or packet.stream_name == 'disparity_color':
-            frame = packet.getData()
-        else:
-            continue
-
         if packet.stream_name == "left":
             latest_left = packet
         elif packet.stream_name == "right" and latest_left is not None and seq(latest_left) == seq(packet):
             lr_pairs[step_norm(tst(packet))] = (latest_left, packet)
         elif packet.stream_name == 'previewout':
+            data = packet.getData()
+            data0 = data[0, :, :]
+            data1 = data[1, :, :]
+            data2 = data[2, :, :]
+            frame = cv2.merge([data0, data1, data2])
+
             timestamp_normalized = step_norm(tst(packet))
             pair = lr_pairs.pop(timestamp_normalized, None)
             if pair is not None:
+                store_frames(pair[0].getData(), pair[1].getData(), frame)
                 cv2.imshow('left', pair[0].getData())
                 cv2.imshow('right', pair[1].getData())
                 cv2.imshow('previewout', frame)
@@ -88,5 +107,7 @@ while True:
     if cv2.waitKey(1) == ord('q'):
         break
 
+for proc in procs:
+    proc.join()
 del p
 depthai.deinit_device()
